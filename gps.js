428d36bf1e05167e9eef57c9745e7bd8dbce779d
ColbySong <colbysong@yahoo.ca>
2015-07-25 22:02:44 -0700

    gps updated

--- gps.js
+++ gps.js
@@ -3,57 +3,34 @@
  */
 Router.route('/gps');
 
-Markers = new Mongo.Collection('markers');
-
 if (Meteor.isClient) {
-    Template.map.onCreated(function() {
-        GoogleMaps.ready('map', function(map) {
-            google.maps.event.addListener(map.instance, 'click', function(event) {
-                Markers.insert({ lat: event.latLng.lat(), lng: event.latLng.lng() });
-            });
-
-            var markers = {};
-
-            Markers.find().observe({
-                added: function (document) {
-                    var marker = new google.maps.Marker({
-                        draggable: true,
-                        animation: google.maps.Animation.DROP,
-                        position: new google.maps.LatLng(document.lat, document.lng),
-                        map: map.instance,
-                        id: document._id
-                    });
-
-                    google.maps.event.addListener(marker, 'dragend', function(event) {
-                        Markers.update(marker.id, { $set: { lat: event.latLng.lat(), lng: event.latLng.lng() }});
-                    });
-
-                    markers[document._id] = marker;
-                },
-                changed: function (newDocument, oldDocument) {
-                    markers[newDocument._id].setPosition({ lat: newDocument.lat, lng: newDocument.lng });
-                },
-                removed: function (oldDocument) {
-                    markers[oldDocument._id].setMap(null);
-                    google.maps.event.clearInstanceListeners(markers[oldDocument._id]);
-                    delete markers[oldDocument._id];
-                }
-            });
-        });
-    });
-
-    Meteor.startup(function() {
+    Meteor.startup(function () {
         GoogleMaps.load();
     });
 
-    Template.map.helpers({
-        mapOptions: function() {
-            if (GoogleMaps.loaded()) {
-                return {
-                    center: new google.maps.LatLng(-37.8136, 144.9631),
-                    zoom: 8
-                };
-            }
-        }
+    Template.body.helpers({
+        exampleMapOptions: function () {
+            // Make sure the maps API has loaded
+
+                var latLng = Geolocation.latLng();
+                var lat = latLng.lat;
+                var lng = latLng.lng;
+                //alert(latLng.latitude)
+                // Map initialization options
+
+               return {
+                    center: new google.maps.LatLng(lat, lng),
+                   zoom: 15,
+                   
+
+               };
+           }
+
+
+
     });
+
+
 }
+
+
