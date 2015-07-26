c9bb522329a92afc160bb928eba54d440e8ab367
ColbySong <colbysong@yahoo.ca>
2015-07-26 00:25:37 -0700

    gps updated with you are here tag

--- gps.js
+++ gps.js
@@ -1,36 +1,3 @@
 /**
  * Created by Colby on 2015-07-25.
  */
-Router.route('/gps');
-
-if (Meteor.isClient) {
-    Meteor.startup(function () {
-        GoogleMaps.load();
-    });
-
-    Template.body.helpers({
-        exampleMapOptions: function () {
-            // Make sure the maps API has loaded
-
-                var latLng = Geolocation.latLng();
-                var lat = latLng.lat;
-                var lng = latLng.lng;
-
-                
-
-               return {
-                    center: new google.maps.LatLng(lat, lng),
-                   zoom: 15
-                   
-
-               };
-           }
-
-
-
-    });
-
-
-}
-
-
