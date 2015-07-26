52d46a07e3d0b4dd060d19ae90075315a9b5d2d9
yoonyok <jyo1031@gmail.com>
2015-07-25 21:57:17 -0700

    Routing

--- timer.js
+++ timer.js
@@ -2,43 +2,75 @@ Router.route('/timer');
 
 if (Meteor.isClient) {
 
-var $time;
-var clocktimer;
+    var $time;
+    var clocktimer;
 
-    var time = 0;
+
+    var second2 = 0;
+    var second1 = 0;
+    var minute2 = 0;
+    var minute1 = 0;
+    var hour2 = 0;
+    var hour1 = 0;
     var inc = 0;
+    var div = hour1 + "" + hour2 + ":" + minute1 + minute2 + ":" + second1 + second2;
+
 
     Template.foo.helpers({
-        timer : function() {
+        timer: function () {
             Chronos.liveUpdate();
-            time +=  inc;
-            return time;
+            second2 += inc;
+            if (second2 > 9) {
+                second2 = 0;
+                second1 += 1;
+            }
+            if (second1 > 5) {
+                second1 = 0;
+                minute2 += 1;
+            }
+            if (minute2 > 9) {
+                minute2 = 0;
+                minute1 += 1;
+            }
+            if (minute1 > 5) {
+                minute1 = 0;
+                hour2 += 1;
+            }
+            if (hour2 > 9) {
+                hour2 = 0;
+                hour1 += 1;
+            }
+            div = hour1 + "" + hour2 + ":" + minute1 + minute2 + ":" + second1 + second2;
+            return div;
         }
     });
 
     Template.timer.events({
-        'click #start': function(){
+        'click #start': function () {
             timer : {
                 inc = 1;
-                return time;
+                div = hour1 + "" + hour2 + ":" + minute1 + minute2 + ":" + second1 + second2;
+                return div;
             }
         },
 
-        'click #stop': function(){
+        'click #stop': function () {
             timer : {
                 inc = 0;
-                return time;
+                div = hour1 + "" + hour2 + ":" + minute1 + minute2 + ":" + second1 + second2;
+                return div;
             }
         },
 
-        'click #reset': function(){
+        'click #reset': function () {
             timer : {
-                time = 0;
-                return time;
+                second2 = 0;
+                return div;
             }
         }
     });
 }
+
 
 
 
