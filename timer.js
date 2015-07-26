8fb81ced21fbebe379a3d5767525fd46ffdff51a
yoonyok <jyo1031@gmail.com>
2015-07-25 20:23:29 -0700

    Routing

--- timer.js
+++ timer.js
@@ -1,1 +1,44 @@
 Router.route('/timer');
+
+if (Meteor.isClient) {
+
+var $time;
+var clocktimer;
+
+    var time = 0;
+    var inc = 0;
+
+    Template.foo.helpers({
+        timer : function() {
+            Chronos.liveUpdate();
+            time +=  inc;
+            return time;
+        }
+    });
+
+    Template.timer.events({
+        'click #start': function(){
+            timer : {
+                inc = 1;
+                return time;
+            }
+        },
+
+        'click #stop': function(){
+            timer : {
+                inc = 0;
+                return time;
+            }
+        },
+
+        'click #reset': function(){
+            timer : {
+                time = 0;
+                return time;
+            }
+        }
+    });
+}
+
+
+
